21aadf2b956b708003b920da18eec28f65b05a63
gombb <gombazsi>
2021-04-25 13:41:19 +0200

    Add project files.

---
+++ TimeTable/Models/DAL/Lesson.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeTable.Models.DAL
+{
+    public class Lesson
+    {
+        public int LessonId { get; set; }
+        public int DayOfWeek { get; set; }
+        public int LessonNumber { get; set; }
+        public int SubjectId { get; set; }
+        public Subject Subject { get; set; }
+        public int LocationId { get; set; }
+        public Location Location { get; set; }
+    }
+}
